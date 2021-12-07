11e796f555016831c35f7c25875d6fd40d921fe6
bichanna <nobu.bichanna@gmail.com>
2021-12-07 10:01:15 -0600

    Move the functions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = "0.0.5"
+VERSION = "0.0.8"
 DESCRIPTION = 'Basic auth for Django'
 README = os.path.join(os.path.dirname(__file__), "README.md")
 
