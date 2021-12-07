59e28e1b45f4750af30322912edd93dd524e0200
bichanna <nobu.bichanna@gmail.com>
2021-12-07 09:27:15 -0600

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = "0.0.1"
-DESCRIPTION = 'A library for implementing basic auth to API.'
+VERSION = "0.1.0"
+DESCRIPTION = 'Basic auth for Django'
 README = os.path.join(os.path.dirname(__file__), "README.md")
 
 # Setting up
 setup(
-        name="django-basicauth",
+        name="easy-basicauth",
         version=VERSION,
         author="bichanna",
         author_email="nobu.bichanna@gmail.com",
         description=DESCRIPTION,
         long_description=open(README).read(),
         long_description_content_type="text/markdown",
-        url="https://github.com/bichanna/django-basicauth",
+        url="https://github.com/bichanna/django-basic-auth",
         packages=find_packages(),
         install_requires=[
-            "Django",
-			"djangorestframework",
-        ], 
+            "django>=1.8.0",
+			"djangorestframework>=3.0.0",
+        ],
 		license="MIT",
         keywords=["basic-auth", "python", "django", "api"],
         classifiers= [
